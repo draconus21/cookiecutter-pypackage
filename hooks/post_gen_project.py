89a768b0d469142426713e242e5608406886ecd3
Neeth Kunnath <neeth.kunnath@airy3d.com>
2023-03-04 21:59:14 -0500

    remove unwanted files and dirs

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import os
+import shutil
+import fnmatch
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -8,7 +10,29 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
+def remove_dir(dirname):
+    shutil.rmtree(os.path.join(PROJECT_DIRECTORY, dirname))
+
+
+dirs_to_remove = [".vscode", ".undo"]
+files_to_remove = ["Session.vim", "*.code-workspace"]
+
+
+def isMatch(test, pattern_list):
+    for patt in pattern_list:
+        if fnmatch.fnmatch(test, patt):
+            return True
+    return False
+
+
 if __name__ == "__main__":
+    # remove files and directories that match  `files_to_remove` and `dirs_to_remove` respectively
+    for f in os.listdir(PROJECT_DIRECTORY):
+        if isMatch(f, files_to_remove):
+            remove_file(f)
+        elif isMatch(f, dirs_to_remove):
+            remove_dir(f)
+
     if "{{ cookiecutter.create_author_file }}" != "y":
         remove_file("AUTHORS.rst")
         remove_file("docs/authors.rst")
