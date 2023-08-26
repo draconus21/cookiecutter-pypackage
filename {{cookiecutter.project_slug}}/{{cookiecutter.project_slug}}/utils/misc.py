622c36e1c1f9eeba7d9ffc5f77d04674b3bc8f44
Neeth Kunnath <neeth.xavier@gmail.com>
2023-08-26 01:33:35 +0000

    updates utils

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/misc.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/misc.py
@@ -7,7 +7,7 @@ from copy import deepcopy
 from pathlib import Path
 from configparser import ConfigParser
 
-from {{ cookiecutter.project_slug }}.utils.logutils import serror, swarn
+from {{ cookiecutter.project_slug }}.utils import logutils
 
 
 def userInput(prompt, castFunc=None):
@@ -19,7 +19,7 @@ def userInput(prompt, castFunc=None):
         try:
             var = castFunc(input(f"{prompt}: "))
         except Exception as e:
-            serror(e)
+            logutils.error(e)
             pass
     return var
 
@@ -36,8 +36,7 @@ def mkdirs(fname, deleteIfExists=False):
             confirm = input(f"{p} exists. Do you want to delete it and create an empty directory? (Y/N)")
             if confirm in ["Y", "y"]:
                 shutil.rmtree(p)
-                swarn(f"Deleted directory {p}")
-                logging.info(f"Deleted directory {p}")
+                logutils.info(f"Deleted directory {p}")
                 mkdirs(fname=p, deleteIfExists=deleteIfExists)
         else:
             return True
@@ -110,3 +109,9 @@ def cliToDict(ctx, param, dictStr):
         logging.error(f"Caught {e} while processing for {param}\n{dictStr}")
         raise e
     return jdict
+
+
+def get_dir_from_env(key):
+    d = os.environ.get(key, os.curdir)
+    logutils.debug(f"Fetched {d} for {key}")
+    return d
